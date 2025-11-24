9d614a26d8dd5ee305ddf02288ddd4fbbdab8506
lkuci <lkucikowicz@gmail.com>
2025-11-24 14:54:32 +0100

    setup projektu

---
+++ otrs-backend/otrs-backend/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace otrs_backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Bio { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
