ec7672a089aad6c70754c27868634fcec03298f9
pamix00 <skkfos@gmail.com>
2026-03-11 14:48:09 +0100

    fix: update error messages in login/register page

--- otrs-frontend/src/views/LoginRegisterPage.vue
+++ otrs-frontend/src/views/LoginRegisterPage.vue
@@ -91,12 +91,15 @@
                   <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.243 4.243l-4.243-4.243" /></svg>
                 </button>
               </div>
-                <p v-if="errorMessage" class="text-red-500 text-sm mt-1">
-                  {{ errorMessage }}
-                </p>
             </div>
           </div>
         </transition>
+
+        <div class="h-6 mb-1">
+          <p v-if="errorMessage" class="text-red-500 text-sm mt-1">
+            {{ errorMessage }}
+          </p>
+        </div>
 
         <button 
           type="submit" 
@@ -126,7 +129,7 @@
 </template>
 
 <script setup>
-import { reactive, ref, computed } from 'vue';
+import { reactive, ref, computed, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
@@ -134,6 +137,10 @@ const router = useRouter();
 
 const activeTab = computed(() => {
   return route.name === 'register' ? 'register' : 'login';
+});
+
+watch(activeTab, () => {
+  errorMessage.value = '';
 });
 
 const showPassword = ref(false);
